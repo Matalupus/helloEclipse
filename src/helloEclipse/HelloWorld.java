ab23d8af4c5fbce73f8ab8438394905c3e6a15ac
gimbrailo <ginoimbrailo@gmail.com>
2017-05-26 10:41:13 +0200

    Added the HelloWorld class

---
+++ src/helloEclipse/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloEclipse;
+
+public class HelloWorld {
+	
+	public static String say(String name) {
+		return "Hello, " + name;
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println(say("Gino"));
+
+	}
+
+}
